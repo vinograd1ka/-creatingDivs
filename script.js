e921edfda70467362695b848560c3e53feb120fc
vinograd1ka <kachanov220@mail.ua>
2020-12-08 16:04:36 +0200

    almost done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function creatingDivs(){
+
     var number = document.getElementById('number');
     var width = document.getElementById('width');
     var height = document.getElementById('height');
@@ -7,8 +8,12 @@
     var createButton = document.querySelector('.btn-defaults');
     var deleteButton = document.querySelector('.btn-delete');
     var container = document.querySelector('.div-container');
+    var deleteLast2ndButton = document.querySelector('.btn-delete-2nd')
+    var changeLastColorButton = document.querySelector('.btn-change-last-color')
+    var changeColorsButton = document.querySelector('.btn-change-colors')
 
     createButton.addEventListener('click', function () {
+        container.innerHTML = '';
         for(let i = 0; i < number.value; i++){
             var div = document.createElement("div");
             div.className = "created-div"
@@ -39,7 +44,39 @@
             var allDivs = document.querySelectorAll('.created-div');
 
             if (i%2===0)allDivs[i].remove()
-            //allDivs[i%3==0].remove();
         }
     });
+
+    deleteLast2ndButton.addEventListener('click', function () {
+            var allDivs = document.querySelectorAll('.created-div');
+            allDivs[allDivs.length - 1].remove();
+            allDivs[allDivs.length - 2].remove();
+    });
+
+    changeLastColorButton.addEventListener('click', function () {
+         var allDivs = document.querySelectorAll('.created-div');
+         var lastElement = allDivs[allDivs.length - 1];
+
+         if(window.getComputedStyle(lastElement).backgroundColor === 'rgb(255,255,255)'){
+              lastElement.style.background = 'rgb(255, 255, 0)'
+
+          }else if (window.getComputedStyle(lastElement).backgroundColor === 'rgb(255, 255, 0)') {
+              lastElement.style.backgroundColor = 'rgb(0, 255, 0)'
+
+          }else if (window.getComputedStyle(lastElement).backgroundColor === 'rgb(0, 255, 0)') {
+              lastElement.style.backgroundColor = 'rgb(0, 0, 255)'
+          }else if (window.getComputedStyle(lastElement).backgroundColor === 'rgb(0, 0, 255)'){
+              lastElement.style.backgroundColor = 'rgb(255, 255, 255)'
+          }
+    });
+
+
+    changeColorsButton.addEventListener('click', function () {
+        var allDivss = document.querySelectorAll('.created-div');
+
+        for(let i = 0; i < allDivss.length; i--) {
+
+        }
+    });
+
 })()
